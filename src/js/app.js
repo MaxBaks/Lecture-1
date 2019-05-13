322ec758dbdbf7dc1fc1f0bbee32511cec3dc08d
MaxBaks <maximkakb24@gmail.com>
2019-05-13 12:52:51 +0300

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,5 @@
-"use strict";
 
 const prices = [200, 550, 4000, 23, 58, 5000, 485, 711];
-
-for (let price of prices) {
-    const itemIndex = prices.indexOf(price) + 1;
-    $('#pricesContainer').append(`<input type='checkBox' onchange='onChoiceChanged()' value='${price}' id='item${itemIndex}'/>` +
-                                 `<label for='item${itemIndex}'>Товар ${itemIndex}, цена: ${price}</div><br/>`);
-}
-
-const onChoiceChanged = () => {
-    let selectedItems = [];
-    $.each($("#pricesContainer input[type='checkBox']:checked"), function() {
-        selectedItems.push(Number($(this).val()));
-    });
-    calculateSum(selectedItems);
-}
 
 const calculateSum = (items) => {   
     let sum = 0;
@@ -22,5 +7,12 @@ const calculateSum = (items) => {
         sum += item;
     }
     const sumText = sum > 10000 ? `Бонус 5% - ${Math.floor(sum * 0.05)}, сумма с учетом бонуса - ${sum - Math.floor(sum * 0.05)}` : `Сумма: ${sum}`;
-    $('#summary').html(sumText);
+    return sumText;
 }
+
+for (let price of prices) {
+    const itemIndex = prices.indexOf(price);
+    const selectedPrices = prices.slice(0, itemIndex + 1);
+    console.log(calculateSum(selectedPrices));
+}
+
